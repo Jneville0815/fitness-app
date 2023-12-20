f3cfd0cafd2fa34f7cf54b721666445af2e25cc0
Jimmy Neville <jimmyeneville@gmail.com>
2023-12-20 09:15:08 -0500

    Update backend endpoint

--- src/api/backend.js
+++ src/api/backend.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const localTesting = false
 
 export const backend_url =
-    'https://3cl6ts0f6f.execute-api.us-east-1.amazonaws.com/production/api'
+    'https://ogby0w3cuj.execute-api.us-east-1.amazonaws.com/production/api'
 
 export default axios.create({
     baseURL: localTesting ? 'http://localhost:2000/api' : backend_url,
